d2ed50448d1d069b65a24fc8ae2d430362d8fbfe
pablomata1 <118005870+pablomata1@users.noreply.github.com>
2024-11-01 11:07:43 -0400

    Update scope_lab.js

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -19,13 +19,14 @@ console.log(globalConst); // Output: "I'm a global constant"
 //console.log(blockVar);
 //console.log(blockLet);
 
+//Function scope
 function show(){
     var functionVar = "I'm a block-scoped var";
     let functionLet = "I'm a block-scoped let";
     const functionConst = "I'm a block-scoped const";
 }
 show();
-    
+//Function scope    
 console.log(functionVar); // Throws ReferenceError
 console.log(functionLet); // Throws ReferenceError
 console.log(functionConst); // Throws ReferenceError
