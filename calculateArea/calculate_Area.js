5094b5a2df634871775f1e3bda430564650b10be
pablomata1 <pablomata977@gmail.com>
2024-11-01 15:00:14 -0400

    added new functionality

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -15,3 +15,18 @@ function calculateArea(){
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateAmount(){
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let amount = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('resultAmount').innerText = `The total amount is: $ ${amount}`;
+}
