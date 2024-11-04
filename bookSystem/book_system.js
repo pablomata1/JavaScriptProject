24c285c1ff120ec88d9b6e657a14efdb85b0e29d
pablomata1 <pablomata977@gmail.com>
2024-11-04 13:33:40 -0500

    adding delete functionality

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,13 +26,20 @@ function addBook() {
         }
 }
 
+//Delete book
+function deleteBook(index){
+    books.splice(index, 1);
+    showbooks();
+}
+
 //Display all books 
 function showbooks() {
     const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
     );
     
     document.getElementById('books').innerHTML = booksDiv.join('');
